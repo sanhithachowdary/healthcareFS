5551dc2cf034d908a003701f25d7ebaac0fe5a77
HARSHITHAGUDURU <144054914+HARSHITHAGUDURU@users.noreply.github.com>
2025-10-08 22:58:39 +0530

    Update PatientHome.jsx

--- HMS-FE/Frontend/healthcaresdp/src/patient/PatientHome.jsx
+++ HMS-FE/Frontend/healthcaresdp/src/patient/PatientHome.jsx
@@ -11,7 +11,9 @@ export default function PatientHome() {
   const handleViewDoctors = () => {
     navigate('/doctors');
   };
-
+const handleBookAppointment = () => {
+    navigate('/book-appointment');
+  };
   const handleMyAppointments = () => {
     navigate('/my-appointments');
   };
