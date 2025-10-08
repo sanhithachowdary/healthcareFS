f15b2618907be1ba92e0513998db448d92178a66
GopiSivaalasetty28 <2300033411cse2@gmail.com>
2025-10-08 23:02:24 +0530

    Update DoctorLogin.jsx

--- HMS-FE/Frontend/healthcaresdp/src/doctor/DoctorLogin.jsx
+++ HMS-FE/Frontend/healthcaresdp/src/doctor/DoctorLogin.jsx
@@ -3,8 +3,5 @@ import React from 'react'
 export default function DoctorLogin() {
   return (
     <div>DoctorLogin</div>
-    export default function DoctorLogin() {
-  return (
-    <div>DoctorLogin</div>
   )
 }
